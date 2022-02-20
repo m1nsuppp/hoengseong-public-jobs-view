340f67db9bb07c8dc9bb3f4abcdca63bd62bba23
m1nsuppp <m1nsuppp@gmail.com>
2022-02-20 14:04:48 +0900

    renew

---
+++ assets/js/menu.js
@@ -0,0 +1,15 @@
+const schoolList = document.querySelector('#schoolList');
+const elementarySchool = schoolList.querySelector('#elementarySchool');
+const middleSchool = schoolList.querySelector('#middleSchool');
+const highSchool = schoolList.querySelector('#highSchool');
+
+middleSchool.addEventListener('click', () => {
+
+});
+
+const inner = document.querySelector('#main .inner');
+
+const styles = inner.querySelectorAll('.tiles article');
+
+styles.forEach(style => {
+});
